a946ed75f54ea91b6da3e929ea792f0f236ffdfb
Codenames-Ren <renmizuki38@gmail.com>
2025-07-14 23:37:32 +0700

    nambah cors

--- src/app/main.py
+++ src/app/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
 from rembg import remove
 from io import BytesIO
+from fastapi.middleware.cors import CORSMiddleware
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="127.0.0.1", port=1080, reload=True)
+
 
 app = FastAPI()
+
+#CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #change this origins in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/remove-bg")
 async def remove_bg(file: UploadFile = File(...)):
