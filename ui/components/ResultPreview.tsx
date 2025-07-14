ce6398eb37eb20500c880aa58c5279510d56501e
Codenames-Ren <renmizuki38@gmail.com>
2025-07-14 23:14:01 +0700

    ganti style sedikit

--- ui/components/ResultPreview.tsx
+++ ui/components/ResultPreview.tsx
@@ -3,7 +3,9 @@
 export default function ResultPreview({ imageUrl }: { imageUrl: string }) {
   return (
     <div className="mt-4">
-      <h2 className="font-bold text-lg mb-2">Result Preview</h2>
+      <h2 className="font-bold text-lg mb-2 text-white text-shadow-[2px_2px_0px_black]">
+        Result Preview :{" "}
+      </h2>
       <img
         src={imageUrl}
         alt="Processed"
@@ -12,7 +14,7 @@ export default function ResultPreview({ imageUrl }: { imageUrl: string }) {
       <a
         href={imageUrl}
         download="output.png"
-        className="inline-block mt-3 px-4 py-2 bg-black text-white rounded"
+        className="flex mt-3 px-4 py-2 bg-blue-700 shadow-[6px_6px_0px_black] hover:shadow-[1px_1px_0px_black] transition-all duration-200 text-white rounded justify-center"
       >
         Download
       </a>
